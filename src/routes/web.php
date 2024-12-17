102ed6d84f1ff4caf2c140b054d16a13131cb956
nanami <upuoup135@yahoo.co.jp>
2024-12-18 00:04:53 +0900

    変更の更新241217

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    // ルートをProductControllerのindexメソッドに変更
+    Route::get('/', [ProductController::class, 'index'])->name('products.index');
+    
+    // products.indexルートを定義
+    Route::get('/products', [ProductController::class, 'index'])->name('products.index');
+    
+    // products.createルートを定義
+    Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+    Route::get('/sell', [ProductController::class, 'create'])->name('products.create'); // 出品画面
+    Route::post('/products', [ProductController::class, 'store'])->name('products.store'); // 商品登録
 });

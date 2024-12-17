6fb149b1fce0472911b34e79601282456583cbc0
nanami <upuoup135@yahoo.co.jp>
2024-12-18 00:24:20 +0900

    変更の更新241217

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,14 +5,14 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 
 Route::middleware('auth')->group(function () {
-    // ルートをProductControllerのindexメソッドに変更
-    Route::get('/', [ProductController::class, 'index'])->name('products.index');
-    
+    // ホーム画面を商品一覧画面に変更
+    Route::get('/', [ProductController::class, 'index'])->name('home');
+
     // products.indexルートを定義
     Route::get('/products', [ProductController::class, 'index'])->name('products.index');
     
     // products.createルートを定義
     Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-    Route::get('/sell', [ProductController::class, 'create'])->name('products.create'); // 出品画面
+    Route::get('/sell', [ProductController::class, 'create'])->name('products.sell'); // 出品画面
     Route::post('/products', [ProductController::class, 'store'])->name('products.store'); // 商品登録
 });
